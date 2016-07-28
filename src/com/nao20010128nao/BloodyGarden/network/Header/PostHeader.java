98f2e4b5368a3d044691912fbae0a5f64e4e57b6
nao20010128nao <nao20010128@gmail.com>
2016-07-28 23:03:23 +0900

    フォーマット

--- src/com/nao20010128nao/BloodyGarden/network/Header/PostHeader.java
+++ src/com/nao20010128nao/BloodyGarden/network/Header/PostHeader.java
@@ -1,57 +1,57 @@
 package com.nao20010128nao.BloodyGarden.network.Header;
 
 public class PostHeader {
-	public String Host= "";
+	public String Host = "";
 	public boolean Connection = false;
-	public String Accept= "";
-	public String UserAgent= "";
-	public String Referer= "";
-	public String AcceptEncoding= "";
-	public String AcceptLanguage= "";
-	public String Origin= "";
-	public String ContentType= "";
-	
-	public PostHeader setHost(String host){
+	public String Accept = "";
+	public String UserAgent = "";
+	public String Referer = "";
+	public String AcceptEncoding = "";
+	public String AcceptLanguage = "";
+	public String Origin = "";
+	public String ContentType = "";
+
+	public PostHeader setHost(String host) {
 		this.Host = host;
 		return this;
 	}
-	
-	public PostHeader setConnection(boolean connection){
+
+	public PostHeader setConnection(boolean connection) {
 		this.Connection = connection;
 		return this;
 	}
-	
-	public PostHeader setAccept(String accept){
+
+	public PostHeader setAccept(String accept) {
 		this.Accept = accept;
 		return this;
 	}
-	
-	public PostHeader setUserAgent(String useragent){
+
+	public PostHeader setUserAgent(String useragent) {
 		this.UserAgent = useragent;
 		return this;
 	}
-	
-	public PostHeader setReferer(String referer){
+
+	public PostHeader setReferer(String referer) {
 		this.Referer = referer;
 		return this;
 	}
-	
-	public PostHeader setAcceptEncoding(String encoding){
+
+	public PostHeader setAcceptEncoding(String encoding) {
 		this.AcceptEncoding = encoding;
 		return this;
 	}
-	
-	public PostHeader setAcceptLanguage(String language){
+
+	public PostHeader setAcceptLanguage(String language) {
 		this.AcceptLanguage = language;
 		return this;
 	}
-	
-	public PostHeader setOrigin(String origin){
+
+	public PostHeader setOrigin(String origin) {
 		this.Origin = origin;
 		return this;
 	}
-	
-	public PostHeader setContentType(String contenttype){
+
+	public PostHeader setContentType(String contenttype) {
 		this.ContentType = contenttype;
 		return this;
 	}
--- src/com/nao20010128nao/BloodyGarden/network/Http.java
+++ src/com/nao20010128nao/BloodyGarden/network/Http.java
@@ -16,128 +16,130 @@ import com.nao20010128nao.BloodyGarden.network.Header.GetHeader;
 import com.nao20010128nao.BloodyGarden.network.Header.PostHeader;
 
 public class Http {
-	
+
 	public CookieManager cookie = null;
-	
-	public Http(){
+
+	public Http() {
 		this.cookie = new CookieManager();
 		CookieHandler.setDefault(this.cookie);
 	}
-	
-	public String get(String url, GetHeader header){
+
+	public String get(String url, GetHeader header) {
 		String result = "";
 		try {
 			URI uri = new URI(url);
-			HttpURLConnection connection = (HttpURLConnection)uri.toURL().openConnection();
+			HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
 			connection.setInstanceFollowRedirects(false);
 			connection.setRequestMethod("GET");
-			if(header.Host != "")
+			if (header.Host != "")
 				connection.setRequestProperty("Host", header.Host);
-			if(header.Connection)
+			if (header.Connection)
 				connection.setRequestProperty("Connection", "Keep-Alive");
-			if(header.Accept != "")
+			if (header.Accept != "")
 				connection.setRequestProperty("Accept", header.Accept);
-			if(header.UserAgent != "")
+			if (header.UserAgent != "")
 				connection.setRequestProperty("User-Agent", header.UserAgent);
-			if(header.Referer != "")
+			if (header.Referer != "")
 				connection.setRequestProperty("Referer", header.Referer);
-			if(header.AcceptEncoding != "")
+			if (header.AcceptEncoding != "")
 				connection.setRequestProperty("Accept-Encoding", header.AcceptEncoding);
-			if(header.AcceptLanguage != "")
+			if (header.AcceptLanguage != "")
 				connection.setRequestProperty("Accept-Language", header.AcceptLanguage);
-			
+
 			String location = connection.getHeaderField("Location");
-			if(location != null){
+			if (location != null) {
 				GetHeader redirect_header = new GetHeader();
-				if(header.Host != "")
+				if (header.Host != "")
 					redirect_header.setHost(header.Host);
-				if(header.Connection)
+				if (header.Connection)
 					redirect_header.setConnection(header.Connection);
-				if(header.Accept != "")
+				if (header.Accept != "")
 					redirect_header.setAccept(header.Accept);
-				if(header.UserAgent != "")
+				if (header.UserAgent != "")
 					redirect_header.setUserAgent(header.UserAgent);
-				if(header.Referer != "")
+				if (header.Referer != "")
 					redirect_header.setReferer(header.Referer);
-				if(header.AcceptEncoding != "")
+				if (header.AcceptEncoding != "")
 					redirect_header.setAcceptEncoding(header.AcceptEncoding);
-				if(header.AcceptLanguage != "")
+				if (header.AcceptLanguage != "")
 					redirect_header.setAcceptLanguage(header.AcceptLanguage);
-				result = this.get((location.indexOf("http") != 0 ? url.substring(0, url.lastIndexOf("/")) : "") + location, redirect_header);
-			}
-			else{
+				result = this.get(
+						(location.indexOf("http") != 0 ? url.substring(0, url.lastIndexOf("/")) : "") + location,
+						redirect_header);
+			} else {
 				BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
 				String buffer = reader.readLine();
-				while(buffer != null){
+				while (buffer != null) {
 					result += buffer + "\n";
 					buffer = reader.readLine();
 				}
 			}
 		} catch (URISyntaxException e) {
-			
+
 		} catch (MalformedURLException e) {
-			
+
 		} catch (IOException e) {
-			
+
 		}
 		return result;
 	}
-	
-	public String post(String url, String data, PostHeader header){
+
+	public String post(String url, String data, PostHeader header) {
 		String result = "";
 		try {
 			byte[] post_data = data.getBytes("UTF-8");
 			URI uri = new URI(url);
-			HttpURLConnection connection = (HttpURLConnection)(uri.toURL().openConnection());
+			HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
 			connection.setDoOutput(true);
 			connection.setInstanceFollowRedirects(false);
 			connection.setRequestMethod("POST");
-			if(header.Host != "")
+			if (header.Host != "")
 				connection.setRequestProperty("Host", header.Host);
-			if(header.Connection)
+			if (header.Connection)
 				connection.setRequestProperty("Connection", "Keep-Alive");
-			if(header.Accept != "")
+			if (header.Accept != "")
 				connection.setRequestProperty("Accept", header.Accept);
-			if(header.Origin != "")
+			if (header.Origin != "")
 				connection.setRequestProperty("Origin", header.Origin);
-			if(header.UserAgent != "")
+			if (header.UserAgent != "")
 				connection.setRequestProperty("User-Agent", header.UserAgent);
-			if(header.ContentType != "")
+			if (header.ContentType != "")
 				connection.setRequestProperty("Content-Type", header.ContentType);
-			if(header.Referer != "")
+			if (header.Referer != "")
 				connection.setRequestProperty("Referer", header.Referer);
-			if(header.AcceptEncoding != "")
+			if (header.AcceptEncoding != "")
 				connection.setRequestProperty("Accept-Encoding", header.AcceptEncoding);
-			if(header.AcceptLanguage != "")
+			if (header.AcceptLanguage != "")
 				connection.setRequestProperty("Accept-Language", header.AcceptLanguage);
-			
+
 			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), "UTF-8"));
 			writer.write(data);
 			writer.close();
-			
+
 			String location = connection.getHeaderField("Location");
-			if(location != null){
+			if (location != null) {
 				GetHeader redirect_header = new GetHeader();
-				if(header.Host != "")
+				if (header.Host != "")
 					redirect_header.setHost(header.Host);
-				if(header.Connection)
+				if (header.Connection)
 					redirect_header.setConnection(header.Connection);
-				if(header.Accept != "")
+				if (header.Accept != "")
 					redirect_header.setAccept(header.Accept);
-				if(header.UserAgent != "")
+				if (header.UserAgent != "")
 					redirect_header.setUserAgent(header.UserAgent);
-				if(header.Referer != "")
+				if (header.Referer != "")
 					redirect_header.setReferer(header.Referer);
-				if(header.AcceptEncoding != "")
+				if (header.AcceptEncoding != "")
 					redirect_header.setAcceptEncoding(header.AcceptEncoding);
-				if(header.AcceptLanguage != "")
+				if (header.AcceptLanguage != "")
 					redirect_header.setAcceptLanguage(header.AcceptLanguage);
-				result = this.get((location.indexOf("http") != 0 ? url.substring(0, url.lastIndexOf("/")) : "") + location, redirect_header);
-			}
-			else{
+				result = this.get(
+						(location.indexOf("http") != 0 ? url.substring(0, url.lastIndexOf("/")) : "") + location,
+						redirect_header);
+			} else {
 				BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
 				String buffer = reader.readLine();
-				while(buffer != null){
+				while (buffer != null) {
 					result += buffer + "\n";
 					buffer = reader.readLine();
 				}
@@ -151,61 +153,62 @@ public class Http {
 		}
 		return result;
 	}
-	
-	public String post_x_www_form_urlencoded(String url, String data, PostHeader header){
+
+	public String post_x_www_form_urlencoded(String url, String data, PostHeader header) {
 		String result = "";
 		try {
 			byte[] data_bytes = data.getBytes("UTF-8");
 			URI uri = new URI(url);
-			HttpURLConnection connection = (HttpURLConnection)(uri.toURL().openConnection());
+			HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
 			connection.setDoOutput(true);
 			connection.setInstanceFollowRedirects(false);
 			connection.setRequestMethod("POST");
-			if(header.Host != "")
+			if (header.Host != "")
 				connection.setRequestProperty("Host", header.Host);
-			if(header.Connection)
+			if (header.Connection)
 				connection.setRequestProperty("Connection", "Keep-Alive");
-			if(header.Accept != "")
+			if (header.Accept != "")
 				connection.setRequestProperty("Accept", header.Accept);
-			if(header.Origin != "")
+			if (header.Origin != "")
 				connection.setRequestProperty("Origin", header.Origin);
-			if(header.UserAgent != "")
+			if (header.UserAgent != "")
 				connection.setRequestProperty("User-Agent", header.UserAgent);
 			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			if(header.Referer != "")
+			if (header.Referer != "")
 				connection.setRequestProperty("Referer", header.Referer);
-			if(header.AcceptEncoding != "")
+			if (header.AcceptEncoding != "")
 				connection.setRequestProperty("Accept-Encoding", header.AcceptEncoding);
-			if(header.AcceptLanguage != "")
+			if (header.AcceptLanguage != "")
 				connection.setRequestProperty("Accept-Language", header.AcceptLanguage);
-			
+
 			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), "UTF-8"));
 			writer.write(data);
 			writer.close();
-			
+
 			String location = connection.getHeaderField("Location");
-			if(location != null){
+			if (location != null) {
 				GetHeader redirect_header = new GetHeader();
-				if(header.Host != "")
+				if (header.Host != "")
 					redirect_header.setHost(header.Host);
-				if(header.Connection)
+				if (header.Connection)
 					redirect_header.setConnection(header.Connection);
-				if(header.Accept != "")
+				if (header.Accept != "")
 					redirect_header.setAccept(header.Accept);
-				if(header.UserAgent != "")
+				if (header.UserAgent != "")
 					redirect_header.setUserAgent(header.UserAgent);
-				if(header.Referer != "")
+				if (header.Referer != "")
 					redirect_header.setReferer(header.Referer);
-				if(header.AcceptEncoding != "")
+				if (header.AcceptEncoding != "")
 					redirect_header.setAcceptEncoding(header.AcceptEncoding);
-				if(header.AcceptLanguage != "")
+				if (header.AcceptLanguage != "")
 					redirect_header.setAcceptLanguage(header.AcceptLanguage);
-				result = this.get((location.indexOf("http") != 0 ? url.substring(0, url.lastIndexOf("/")) : "") + location, redirect_header);
-			}
-			else{
+				result = this.get(
+						(location.indexOf("http") != 0 ? url.substring(0, url.lastIndexOf("/")) : "") + location,
+						redirect_header);
+			} else {
 				BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
 				String buffer = reader.readLine();
-				while(buffer != null){
+				while (buffer != null) {
 					result += buffer + "\n";
 					buffer = reader.readLine();
 				}
@@ -219,65 +222,66 @@ public class Http {
 		}
 		return result;
 	}
-	
-	public String post_form_data(String url, String boundary, String[] data, PostHeader header){
+
+	public String post_form_data(String url, String boundary, String[] data, PostHeader header) {
 		String result = "";
 		try {
 			String post_data = "";
-			for(String cdata : data)
+			for (String cdata : data)
 				post_data += "--" + boundary + "\r\n" + cdata + "\r\n";
-			
+
 			byte[] data_bytes = post_data.getBytes("UTF-8");
 			URI uri = new URI(url);
-			HttpURLConnection connection = (HttpURLConnection)(uri.toURL().openConnection());
+			HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
 			connection.setDoOutput(true);
 			connection.setInstanceFollowRedirects(false);
 			connection.setRequestMethod("POST");
-			if(header.Host != "")
+			if (header.Host != "")
 				connection.setRequestProperty("Host", header.Host);
-			if(header.Connection)
+			if (header.Connection)
 				connection.setRequestProperty("Connection", "Keep-Alive");
-			if(header.Accept != "")
+			if (header.Accept != "")
 				connection.setRequestProperty("Accept", header.Accept);
-			if(header.Origin != "")
+			if (header.Origin != "")
 				connection.setRequestProperty("Origin", header.Origin);
-			if(header.UserAgent != "")
+			if (header.UserAgent != "")
 				connection.setRequestProperty("User-Agent", header.UserAgent);
 			connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-			if(header.Referer != "")
+			if (header.Referer != "")
 				connection.setRequestProperty("Referer", header.Referer);
-			if(header.AcceptEncoding != "")
+			if (header.AcceptEncoding != "")
 				connection.setRequestProperty("Accept-Encoding", header.AcceptEncoding);
-			if(header.AcceptLanguage != "")
+			if (header.AcceptLanguage != "")
 				connection.setRequestProperty("Accept-Language", header.AcceptLanguage);
-			
+
 			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), "UTF-8"));
 			writer.write(post_data);
 			writer.close();
-			
+
 			String location = connection.getHeaderField("Location");
-			if(location != null){
+			if (location != null) {
 				GetHeader redirect_header = new GetHeader();
-				if(header.Host != "")
+				if (header.Host != "")
 					redirect_header.setHost(header.Host);
-				if(header.Connection)
+				if (header.Connection)
 					redirect_header.setConnection(header.Connection);
-				if(header.Accept != "")
+				if (header.Accept != "")
 					redirect_header.setAccept(header.Accept);
-				if(header.UserAgent != "")
+				if (header.UserAgent != "")
 					redirect_header.setUserAgent(header.UserAgent);
-				if(header.Referer != "")
+				if (header.Referer != "")
 					redirect_header.setReferer(header.Referer);
-				if(header.AcceptEncoding != "")
+				if (header.AcceptEncoding != "")
 					redirect_header.setAcceptEncoding(header.AcceptEncoding);
-				if(header.AcceptLanguage != "")
+				if (header.AcceptLanguage != "")
 					redirect_header.setAcceptLanguage(header.AcceptLanguage);
-				result = this.get((location.indexOf("http") != 0 ? url.substring(0, url.lastIndexOf("/")) : "") + location, redirect_header);
-			}
-			else{
+				result = this.get(
+						(location.indexOf("http") != 0 ? url.substring(0, url.lastIndexOf("/")) : "") + location,
+						redirect_header);
+			} else {
 				BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
 				String buffer = reader.readLine();
-				while(buffer != null){
+				while (buffer != null) {
 					result += buffer + "\n";
 					buffer = reader.readLine();
 				}
